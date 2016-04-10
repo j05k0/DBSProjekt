87873309cb1e5ce17d80c2db758ce0f6d10a5e7f
Jozef Valko <valko14@student.fiit.stuba.sk>
2016-04-10 22:14:23 +0200

    changed strings

--- src/Model/Strings.java
+++ src/Model/Strings.java
@@ -4,7 +4,7 @@ public class Strings {
 	static final String JDBC_DRIVER = "org.postgresql.Driver";
 	static final String DB_URL = "jdbc:postgresql://localhost:5432/DBS_Projekt";
 	static final String USER = "postgres";
-	static final String PASS = "RaceDriver21";
+	static final String PASS = "dbs123456";
 	
 	public static String getJdbcDriver() {
 		return JDBC_DRIVER;
